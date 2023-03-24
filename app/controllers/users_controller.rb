5ce58a62339ad520475976bb345ccf90ead35890
Elaine Demetrion <esdemetrion@gmail.com>
2023-03-23 20:39:00 -0400

    Implements Login using Devise gem

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,13 @@
 class UsersController < ApplicationController
 
     def index
-        @users = Station.all.order(identifier: :asc)
+      @users = Station.all.order(identifier: :asc)
 
-        #TO DO: Create user model/database
-      end
+      #TO DO: Create user model/database
+    end
+
+    def register
+      #TODO: replace Station with User (after User model has been created)
+      @user = Station.new
+    end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_02_06_013307) do
+ActiveRecord::Schema[7.0].define(version: 2023_03_24_002101) do
   create_table "bikes", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.integer "identifier"
     t.integer "current_station_id"
@@ -25,6 +25,18 @@ ActiveRecord::Schema[7.0].define(version: 2023_02_06_013307) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "count"
+  end
+
+  create_table "users", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
 end

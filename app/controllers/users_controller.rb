3f0c6f6895d9af12625bdda92516bc4113d1d7c3
Elaine Demetrion <esdemetrion@gmail.com>
2023-03-23 21:01:35 -0400

    Removes cruft
    - removes old register method (covered by devise authentication)
    - Alters text in user#index view

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,13 +1,6 @@
 class UsersController < ApplicationController
 
     def index
-      @users = Station.all.order(identifier: :asc)
-
-      #TO DO: Create user model/database
-    end
-
-    def register
-      #TODO: replace Station with User (after User model has been created)
-      @user = Station.new
+      @users = User.all
     end
 end

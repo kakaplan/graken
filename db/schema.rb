8be2c51a485890ccc681869fff8e4871ffa8e93e
Kira Kaplan <kakaplan@smith.edu>
2023-03-27 17:14:26 -0400

    creates new columns for firstname and lastname in Users db and allows user to input them at the sign up page

--- db/schema.rb
+++ db/schema.rb
@@ -39,6 +39,8 @@ ActiveRecord::Schema[7.0].define(version: 2023_03_26_211713) do
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "firstname"
+    t.string "lastname"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end

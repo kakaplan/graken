b09530b1d3f6a971fa8be3c9b8d3ddfdd8a0dcc3
Elaine Demetrion <esdemetrion@gmail.com>
2023-03-30 20:44:18 -0400

    Can add, edit, delete stations from the website

--- app/models/station.rb
+++ app/models/station.rb
@@ -3,10 +3,11 @@ class Station < ApplicationRecord
   validates_presence_of    :identifier,
                            :name,
                            :address,
-                           :count,
-                           :status
+                           :status,
+                           :longitude,
+                           :latitude
   validates_uniqueness_of  :identifier
   
-  has_many :docked_bikes, class_name: :Bike, foreign_key: :current_station_id
+  has_many :docked_bikes, class_name: :Bike, primary_key: :identifier, foreign_key: :current_station_id
   
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,11 +4,17 @@ Rails.application.routes.draw do
   get 'station_maps/edit'
   
   devise_for :users
+
+  resources :stations do
+    member do
+      get :delete
+    end
+  end
     
   get '/users' => 'users#index'
   get '/register' => 'users#register' 
   get '/bikes' => 'bikes#index'
-  root to: 'stations#index'
+  root to: 'station_maps#index'
 end
 
 

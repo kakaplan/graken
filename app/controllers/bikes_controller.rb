d97b20feb7093f13a34ee40f4a0281c83d9598d8
Elaine Demetrion <esdemetrion@gmail.com>
2023-03-31 16:06:30 -0400

    Website can show bike details

--- app/controllers/bikes_controller.rb
+++ app/controllers/bikes_controller.rb
@@ -1,6 +1,10 @@
 class BikesController < ApplicationController
 
     def index
-        @bikes = Bike.all
-      end
+      @bikes = Bike.all
+    end
+
+    def show
+      @bike = Bike.find(params[:id])
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -10,6 +10,11 @@ Rails.application.routes.draw do
       get :delete
     end
   end
+
+  resources :bikes do
+    member do
+    end
+  end
     
   get '/users' => 'users#index'
   get '/register' => 'users#register' 

5b5473b9cf004e34c617505e3dad9443caf20aa0
Kira Kaplan <kakaplan@smith.edu>
2023-04-01 14:44:52 -0400

    allows user to logout and adds account info to user view

--- config/routes.rb
+++ config/routes.rb
@@ -12,6 +12,8 @@ Rails.application.routes.draw do
   end
     
   get '/users' => 'users#index'
+  get '/users/account_info'
+
   get '/register' => 'users#register' 
   get '/bikes' => 'bikes#index'
   root to: 'station_maps#index'

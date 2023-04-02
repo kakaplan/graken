d226a8de078e857efcb16dce8eb76f5c66412766
Elaine Demetrion <esdemetrion@gmail.com>
2023-04-01 23:34:37 -0400

    verifies that bike current_station_id belongs to a station

--- app/models/bike.rb
+++ app/models/bike.rb
@@ -4,5 +4,13 @@ class Bike < ApplicationRecord
   validates_uniqueness_of  :identifier
   
   belongs_to :current_station, class_name: :Station, foreign_key: :current_station_id, optional: true
+  
+  validate :current_station_exists
+  
+  private
 
+  def current_station_exists
+    return if Station.where(identifier: current_station_id).any?
+    errors.add(:current_station_id, "Station Identifier must belong to an existing station.")
+  end
 end

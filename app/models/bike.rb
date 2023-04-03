3f77bc2c755b9706c9964784287246ee9b817bf4
Elaine Demetrion <esdemetrion@gmail.com>
2023-04-03 19:39:51 -0400

    added numericality validations to bike/station models

--- app/models/bike.rb
+++ app/models/bike.rb
@@ -2,7 +2,8 @@ class Bike < ApplicationRecord
   validates_presence_of    :identifier,
                            :current_station_id
   validates_uniqueness_of  :identifier
-  
+  validates_numericality_of :identifier, :current_station_id
+
   belongs_to :current_station, class_name: :Station, foreign_key: :current_station_id, optional: true
   
   validate :current_station_exists

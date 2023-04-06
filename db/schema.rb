2584ff8f4c2e62ea315d244969e99ad5e6cd949b
Elaine Demetrion <esdemetrion@gmail.com>
2023-04-05 21:39:08 -0400

    Updated seeds.rb, current users are not admin

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_04_03_005106) do
+ActiveRecord::Schema[7.0].define(version: 2023_04_06_012425) do
   create_table "bikes", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.integer "identifier"
     t.integer "current_station_id"
@@ -69,6 +69,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_04_03_005106) do
     t.datetime "updated_at", null: false
     t.string "firstname"
     t.string "lastname"
+    t.boolean "admin"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end

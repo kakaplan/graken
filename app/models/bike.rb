cc129078f2c9288d95f56eb0128c679003424a87
Elaine Demetrion <esdemetrion@gmail.com>
2023-04-08 21:03:28 -0400

    Adds new validations to bikes and stations

--- app/models/bike.rb
+++ app/models/bike.rb
@@ -1,12 +1,14 @@
 class Bike < ApplicationRecord
   validates_presence_of    :identifier,
                            :current_station_id
-  validates_uniqueness_of  :identifier
+  validates_uniqueness_of  :identifier, :theme_name
   
   belongs_to :current_station, class_name: :Station, primary_key: :identifier, foreign_key: :current_station_id, optional: true
   has_many :rental_history, class_name: :Rental, primary_key: :identifier, foreign_key: :bike_id 
   
-  validates_numericality_of :identifier, :current_station_id
+  validates_numericality_of :identifier, :current_station_id, :mileage
+
+  validates_numericality_of :battery, {greater_than_or_equal_to: 0, less_than_or_equal_to: 1}
   
   validate :current_station_exists
   

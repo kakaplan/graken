b8e6daa4fe724d6e2adbc62a8f06863316a018ea
Elaine Demetrion <esdemetrion@gmail.com>
2023-04-15 23:05:54 -0400

    Displays time in user-friendly format in ET

--- app/models/update.rb
+++ app/models/update.rb
@@ -1,3 +1,13 @@
 class Update < ApplicationRecord
     validates_presence_of :title
+
+    def datetime_created
+        datetime = created_at.in_time_zone("Eastern Time (US & Canada)")
+        datetime.strftime("%B %d, %Y")
+    end
+
+    def datetime_modified
+        datetime = updated_at.in_time_zone("Eastern Time (US & Canada)")
+        datetime.strftime("%B %d, %Y at %I:%M %p ET")
+    end
 end

1b1ded4ecd518a4fa5c4fdc858648359ba1bebbe
Elaine Demetrion <esdemetrion@gmail.com>
2023-04-30 20:51:22 -0400

    fixes bug where station map collapsible side bar isn't responsive (Gisel's solution)

--- app/javascript/controllers/index.js
+++ app/javascript/controllers/index.js
@@ -9,27 +9,3 @@ eagerLoadControllersFrom("controllers", application)
 // Lazy load controllers as they appear in the DOM (remember not to preload controllers in import map!)
 // import { lazyLoadControllersFrom } from "@hotwired/stimulus-loading"
 // lazyLoadControllersFrom("controllers", application)
-
-
-//for the station maps collapsable list
-var coll = document.getElementsByClassName("collapsible");
-var i;
-
-for (i = 0; i < coll.length; i++) {
-  coll[i].addEventListener("click", function() {
-    this.classList.toggle("active");
-    var content = this.nextElementSibling;
-    if (content.style.display === "block") {
-      content.style.display = "none";
-    } else {
-      content.style.display = "block";
-    }
-  });
-}
-
-
-//checkboxes: allows for only 1 check
-
-// bike.identifierthemename
-
-
